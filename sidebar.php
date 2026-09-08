1e5f649261254bd90facf88882c05ab983c44be9
gameonline445 <nmbaar78@gmail.com>
2026-09-08 19:10:16 +0530

    Create sidebar.php

---
+++ sidebar.php
@@ -0,0 +1,11 @@
+<div class="d-flex flex-column p-3 bg-white shadow-sm" style="width: 250px; min-height: 100vh;">
+    <h4 class="text-primary font-bold mb-4">RDX LUDO ADMIN</h4>
+    <ul class="nav nav-pills flex-column mb-auto">
+        <li class="nav-item"><a href="index.php" class="nav-link active">Dashboard</a></li>
+        <li><a href="kyc_requests.php" class="nav-link text-dark">KYC Requests</a></li>
+        <li><a href="deposits.php" class="nav-link text-dark">Deposits</a></li>
+        <li><a href="withdrawals.php" class="nav-link text-dark">Withdrawals</a></li>
+        <li><a href="customers.php" class="nav-link text-dark">Customers</a></li>
+        <li><a href="payment_settings.php" class="nav-link text-dark">Payment Settings</a></li>
+    </ul>
+</div>
